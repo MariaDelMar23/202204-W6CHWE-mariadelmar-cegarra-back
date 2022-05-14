304e16d037b4d6dcf95f9bd6f87daaa38cfa557b
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-14 15:53:46 +0200

    Added errors for notFoundError and generalError

---
+++ server/middlewares/errors.js
@@ -0,0 +1,15 @@
+const chalk = require("chalk");
+
+const debug = require("debug")("robots:server:errors");
+
+const notFoundError = (req, res) => {
+  debug(chalk.red(`${req.method} on ${req.params} not found`));
+  res.status(404).json({ msg: "Endpoint not found" });
+};
+
+const generalError = (error, req, res) => {
+  debug(chalk.red(`${error.message}`));
+  res.status(500).json({ msg: "General error" });
+};
+
+module.exports = { notFoundError, generalError };
