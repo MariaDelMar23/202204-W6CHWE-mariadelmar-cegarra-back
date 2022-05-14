a0fafdb5c0fdfd604040735b0d439fb0545c25fa
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-14 15:56:39 +0200

    Added error middlewares to server index

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const { notFoundError, generalError } = require("./middlewares/errors");
 
 const app = express();
 
 const server = () => {
   app.use(morgan("dev"));
   app.use(express.json());
+
+  app.use(notFoundError);
+  app.use(generalError);
 };
 
 module.exports = {
