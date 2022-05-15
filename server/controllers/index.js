515037ea59efcd446840c6c7810dba1eb7d880f4
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-15 14:52:46 +0200

    Created deleteRobot controller function and added to server router

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,10 +1,22 @@
+require("dotenv").config();
+const debug = require("debug")("robots:server:controllers");
+const chalk = require("chalk");
 const Robot = require("../../db/models/Robot");
 
 const getRobots = async (req, res) => {
+  debug(chalk.blueBright("Request to get robots"));
   const robots = await Robot.find();
   res.status(200).json({ robots });
 };
 
+const deleteRobot = async (req, res) => {
+  debug(chalk.blueBright("Request to delete a robot"));
+  const { idRobot } = req.params;
+  await Robot.findByIdAndDelete(idRobot);
+  res.status(200).json({ msg: `Deleted robot with Id ${idRobot}` });
+};
+
 module.exports = {
   getRobots,
+  deleteRobot,
 };
