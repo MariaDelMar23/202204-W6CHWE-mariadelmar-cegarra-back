db983057f3549ef7f95decc1dd106808dabda46d
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-16 23:41:44 +0200

    Created getRobotByid controller function

--- server/routers/robotRouters.js
+++ server/routers/robotRouters.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const { getRobots, deleteRobot, createRobot } = require("../controllers");
+const {
+  getRobots,
+  deleteRobot,
+  createRobot,
+  getRobotById,
+} = require("../controllers");
+const auth = require("../middlewares/auth");
 
 const robotRouter = express.Router();
 
 robotRouter.get("/", getRobots);
-robotRouter.delete("/:idRobot", deleteRobot);
-robotRouter.post("/", createRobot);
+robotRouter.get("/:idRobot", getRobotById);
+robotRouter.delete("/delete/:idRobot", auth, deleteRobot);
+robotRouter.post("/create", auth, createRobot);
 
 module.exports = robotRouter;
